f9500458e2c31a772caa4b01bf838b150d9349c8
Amele9 <amele090044@gmail.com>
2023-11-26 03:13:54 +0600

    Added debug_mode and raise_errors

--- whatsapp_chatbot_python/bot.py
+++ whatsapp_chatbot_python/bot.py
@@ -1,6 +1,7 @@
+import logging
 from typing import NoReturn, Optional
 
-from whatsapp_api_client_python.API import GreenAPI, GreenAPIResponse
+from whatsapp_api_client_python.API import GreenAPI, GreenAPIError
 
 from .manager.router import Router
 
@@ -10,15 +11,30 @@ class Bot:
             self,
             id_instance: str,
             api_token_instance: str,
+            debug_mode: bool = False,
+            raise_errors: bool = True,
             settings: Optional[dict] = None,
             delete_notifications_at_startup: bool = True
     ):
-        self.api = GreenAPI(id_instance, api_token_instance)
+        self.id_instance = id_instance
+        self.api_token_instance = api_token_instance
+        self.debug_mode = debug_mode
+        self.raise_errors = raise_errors
+
+        self.api = GreenAPI(
+            id_instance,
+            api_token_instance,
+            debug_mode=debug_mode,
+            raise_errors=raise_errors
+        )
+
+        self.__prepare_logger()
+        self.logger = logging.getLogger("whatsapp-chatbot-python")
 
         if not settings:
             self._update_settings()
         else:
-            self.__validate_response(self.api.account.setSettings(settings))
+            self.api.account.setSettings(settings)
 
         if delete_notifications_at_startup:
             self._delete_notifications_at_startup()
@@ -30,26 +46,18 @@ class Bot:
             try:
                 response = self.api.receiving.receiveNotification()
 
-                self.__validate_response(response)
-
                 if not response.data:
                     continue
                 response = response.data
 
                 self.router.route_event(response["body"])
 
-                self.__validate_response(
-                    self.api.receiving.deleteNotification(
-                        response["receiptId"]
-                    )
-                )
+                self.api.receiving.deleteNotification(response["receiptId"])
             except KeyboardInterrupt:
                 break
 
     def _update_settings(self) -> Optional[NoReturn]:
         settings = self.api.account.getSettings()
-
-        self.__validate_response(settings)
 
         response = settings.data
 
@@ -61,44 +69,29 @@ class Bot:
                 and outgoing_message_webhook == "no"
                 and outgoing_api_message_webhook == "no"
         ):
-            self.__validate_response(
-                self.api.account.setSettings({
-                    "incomingWebhook": "yes",
-                    "outgoingMessageWebhook": "yes",
-                    "outgoingAPIMessageWebhook": "yes"
-                })
-            )
+            self.api.account.setSettings({
+                "incomingWebhook": "yes",
+                "outgoingMessageWebhook": "yes",
+                "outgoingAPIMessageWebhook": "yes"
+            })
 
     def _delete_notifications_at_startup(self) -> Optional[NoReturn]:
         while True:
             response = self.api.receiving.receiveNotification()
 
-            self.__validate_response(response)
-
             if not response.data:
                 break
 
-            self.__validate_response(
-                self.api.receiving.deleteNotification(
-                    response.data["receiptId"]
-                )
-            )
+            self.api.receiving.deleteNotification(response.data["receiptId"])
 
-    @staticmethod
-    def __validate_response(response: GreenAPIResponse) -> Optional[NoReturn]:
-        if response.code != 200:
-            if response.error:
-                raise GreenAPIError(response.error)
-            raise GreenAPIError(
-                f"GreenAPI error occurred with status code {response.code}"
-            )
+    def __prepare_logger(self) -> None:
+        if self.debug_mode:
+            logging.basicConfig(level=logging.DEBUG)
+        else:
+            logging.basicConfig(level=logging.INFO)
 
 
 class GreenAPIBot(Bot):
-    pass
-
-
-class GreenAPIError(Exception):
     pass
 
 
