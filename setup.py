f9dc30f0ca29922c5793032fa6ef04c7d88d1b9e
karmaplush <karmaplush@gmail.com>
2024-06-03 17:37:37 +0500

    Python 3.7 removed as unsupported version

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3 :: Only",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -45,5 +44,5 @@ setup(
         "whatsapp-api-client-python==0.0.45",
         "whatsapp-api-webhook-server-python-v2==0.1.0",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
